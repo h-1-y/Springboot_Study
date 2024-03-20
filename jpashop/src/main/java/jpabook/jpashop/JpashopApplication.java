ed0b803528201dfa7d678df2baf67780af0180fd
h1y <h1y@localhost>
2024-03-20 22:48:05 +0900

    프로젝트 환경 셋팅 및 테스

--- jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
+++ jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -7,7 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpashopApplication {
 
 	public static void main(String[] args) {
+		
+		Hello hello = new Hello();
+		
+		hello.setHello("hello");
+		System.out.println("hello.getHello() ======== " + hello.getHello());
+		
 		SpringApplication.run(JpashopApplication.class, args);
+		
 	}
 
 }
