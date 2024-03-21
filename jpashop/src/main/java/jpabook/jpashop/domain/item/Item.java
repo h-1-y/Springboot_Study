724313190925afc6898653e26d41ce5f22f12bff
h1y <h1y@localhost>
2024-03-21 19:36:27 +0900

    JPA 활용 테이블 설계

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -0,0 +1,39 @@
+package jpabook.jpashop.domain.item;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.ManyToMany;
+import jpabook.jpashop.domain.Category;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE) // 한 테이블에 다 때려박는다 
+@DiscriminatorColumn(name = "dtype")
+@Getter
+@Setter
+public abstract class Item {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "item_id")
+	private Long id;
+	
+	private String name;
+	
+	private int price;
+	
+	private int stockQuantity;
+	
+	@ManyToMany(mappedBy = "items")
+	private List<Category> categories = new ArrayList<>();
+	
+}
