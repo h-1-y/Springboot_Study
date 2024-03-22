da95a8e56647cadd2dc60dcb603789ce8dd0e84a
h1y <h1y@localhost>
2024-03-22 16:13:02 +0900

    연관관계 메서드 추가

--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -34,10 +35,10 @@ public class Order {
 	@JoinColumn(name = "member_id") // 조인을 어떤 컬럼으로 사용할 것인지 
 	private Member member;
 	
-	@OneToMany(mappedBy = "order")
+	@OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
 	private List<OrderItem> orderItems = new ArrayList<>();
 	
-	@OneToOne(fetch = FetchType.LAZY)
+	@OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
 	@JoinColumn(name = "delivery_id")
 	private Delivery delivery;
 	
@@ -45,5 +46,28 @@ public class Order {
 	
 	@Enumerated(EnumType.STRING)
 	private OrderStatus status; // 주문상태 ORDER CANCEL
+	
+	
+	// 연관관계 메서드 
+	public void setMember(Member member) {
+		
+		this.member = member;
+		member.getOrders().add(this);
+		
+	}
+	
+	public void addOrderItem(OrderItem orderItem) {
+		
+		orderItems.add(orderItem);
+		orderItem.setOrder(this);
+		
+	}
+	
+	public void setDelivery(Delivery delivery) {
+		
+		this.delivery = delivery;
+		delivery.setOrder(this);
+		
+	}
 	
 }
