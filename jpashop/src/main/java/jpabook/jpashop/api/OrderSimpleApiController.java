09664847d5e50e9a2b71eb882290fb86ea82cdf1
h-1-y <h-1-y@naver.com>
2024-04-11 19:45:42 +0900

    Lazy 강제 초기화

--- jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -24,11 +24,23 @@ public class OrderSimpleApiController {
 
 	private final OrderRepository orderRepository;
 	
-	// entity 를 직접 노출 하면 무슨 문제가 있는지 체험 
+	// entity 직접 노출 
+	// entity 직접 노출 하면 무슨 문제가 있는지 체험
+	// Hibernate5Module 사용
 	@GetMapping("/api/v1/simple-orders")
 	public List<Order> ordersV1() {
 		
 		List<Order> all = orderRepository.findAllByString(new OrderSearch());
+		
+		// Lazy 강제 초기화 
+		// loop를 통해 프록시 객체가 아닌 실제 entity 객체에 접근하므로써 강제로 
+		// 필요한 데이터를 초기화 
+		for ( Order order : all ) {
+			
+			order.getMember().getName();
+			order.getDelivery().getAddress();
+			
+		}
 		
 		return all;
 		
